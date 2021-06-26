1b9b6e8670545a80acfd196e0aedca6ffa0cd5af
KARTHIKRAJUKANDHULA <karthik901069@gmail.com>
2021-06-26 22:25:02 +0530

    First commit

---
+++ GitVersionDemo1/src/Test.java
@@ -0,0 +1,12 @@
+
+public class Test {
+	public static void main(String[] args) {
+		int i=0;
+		if(i==0) {
+			System.out.println("this is zero");
+		}
+		else {
+			System.out.println("non zero");
+		}
+	}
+}
